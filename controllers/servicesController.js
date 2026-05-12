4a9559e896b671432ec6d56e75dbc2924eebb1d4
Matko <matkoscab752@gmail.com>
2026-05-12 15:38:03 -0300

    controller y router funcional

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,36 +1,35 @@
 import fs from "fs/promises"; // Import del módulo de promesas
 
-export async function getServicios(req, res) {
+export async function getServices(req, res) {
   try {
-    const data = await fs.readFile("./data/servicios.json", "utf-8"); // Se lee el archivo de servicios
+    const data = await fs.readFile("./data/services.json", "utf-8"); // Se lee el archivo de servicios
 
-    const servicios = JSON.parse(data); // Convertir el JSON en objeto JavaScript
+    const services = JSON.parse(data); // Convertir el JSON en objeto JavaScript
 
-    res.json(servicios); // Se envia el objeto JSON como response
+    res.json(services); // Se envia el objeto JSON como response
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: "Error al obtener los servicios" });
-    // Si ocurre un error al intentar parsear el JSON, se envia una respuesta de error.
+    res.status(500).json({ msg: "Error al obtener los servicios" }); // Si ocurre un error al intentar parsear el JSON, se envia una respuesta de error 500.
   }
 }
 
-export async function getServicioById(req, res) {
+export async function getServiceById(req, res) {
   try {
     const { id } = req.params; // Se obtiene el ID del servicio desde los parámetros de la ruta
 
-    const data = await fs.readFile("./data/servicios.json", "utf-8"); // Se lee el archivo de servicios
+    const data = await fs.readFile("./data/services.json", "utf-8"); // Se lee el archivo de servicios
 
-    const servicios = JSON.parse(data); // Convertir el JSON en objeto JavaScript
+    const services = JSON.parse(data); // Convertir el JSON en objeto JavaScript
 
-    const servicio = servicios.find((s) => s.id === parseInt(id)); // Se busca el servicio con el ID especificado
+    const service = services.find((s) => s.id === parseInt(id)); // Se busca el servicio con el ID especificado
 
-    if (!servicio) {
+    if (!service) {
       return res.status(404).json({ msg: "Servicio no encontrado" }); // Si no se encuentra el servicio, se envia una respuesta de error 404
     }
 
-    res.json(servicio); // Se envia el servicio encontrado como response
+    res.json(service); // Se envia el servicio encontrado como `response`
   } catch (error) {
     console.error(error);
-    res.status(500).json({ msg: "Error al obtener el servicio" }); // Si ocurre un error al intentar parsear el JSON, se envia una respuesta de error.
+    res.status(500).json({ msg: "Error al obtener el servicio" }); // Si ocurre un error al intentar parsear el JSON, se envia una respuesta de error 500.
   }
 }
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,13 +1,13 @@
 import express from "express"; // Importar Express
 import {
-  getServicios,
-  getServicioById,
+  getServices,
+  getServiceById,
 } from "../controllers/servicesController.js"; // Importar controller de servicios
 
 const router = express.Router(); // Crea la instancia del router de Express
 
-router.get("/", getServicios); // Ruta a GET para obtener todos los servicios, usando getServicios del controller
+router.get("/", getServices); // Ruta a GET para obtener todos los servicios, usando getServices del controller
 
-router.get("/:id", getServicioById); // Ruta a GET para obtener un servicio por ID, usando getServicioById del controller
+router.get("/:id", getServiceById); // Ruta a GET para obtener un servicio por ID, usando getServiceById del controller
 
 export default router; // Exportar el router para ser usado en app.js
